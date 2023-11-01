cdedf68b1b01da2fd70c58481aa53aa4347be474
khurram <khurramali63@gmail.com>
2023-11-01 15:27:58 +0500

    custom arrays class for practice

--- array.java
+++ array.java
@@ -1,20 +1,45 @@
 public class array {
     private int[] items;
     private int count;
-    public array(int length){
+
+    public array(int length) {
         items = new int[length];
     }
-  
 
-    public void print(){
-        for(int i=0;i<count;i++){
+    public void print() {
+        for (int i = 0; i < count; i++) {
             System.out.println(items[i]);
         }
     }
-    public void add(int item){
-        if(count<items.length){
-            items[count]=item;
-            count++;
+            public void removeat(int index){
+                if(index < 0 || index >= count){
+                    throw new IllegalArgumentException();
+                }
+                for(int i=index;i<count;i++){
+                    items[i] = items[i+1];
+
+                }
+                count--;
+            }
+    public void insert(int item) {
+
+        if (count == items.length) {
+            int[] newItems = new int[count * 2];
+            for (int i = 0; i < items.length; i++) {
+                newItems[i] = items[i];
+            }
+            items = newItems;
         }
+
+        items[count] = item;
+        count++;
+    }
+    public int  indexof(int item){
+        for (int i = 0; i < count; i++) {
+            if (items[i] == item) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
--- main.java
+++ main.java
@@ -1,10 +1,12 @@
 public class main {
     public static void main(String[] args) {
          array numbers = new array(3);
-         numbers.add(3);
-         numbers.add(33);
-         numbers.add(33);
-         numbers.add(33);
+         numbers.insert(0);
+         numbers.insert(1);
+         numbers.insert(2);
+         numbers.insert(3);
+        //  numbers.removeat(3);
+        System.out.println(numbers.indexof(044 ));
          numbers.print();
 
     }
